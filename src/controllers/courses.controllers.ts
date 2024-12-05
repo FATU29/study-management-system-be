09e46208d491b1dd8ed6a32f43b300b171097a02
FAT29 <lqm550@gmail.com>
2024-12-06 03:05:27 +0700

    Update feature courses

--- src/controllers/courses.controllers.ts
+++ src/controllers/courses.controllers.ts
@@ -83,3 +83,135 @@ export const deleteCourseController = async (req: Request<ParamsDictionary, any,
     data: result
   })
 }
+
+export const getCourseForAdminController = async (req: Request, res: Response) => {
+  const data = await courseServices.getCourseForAdminService()
+
+  res.json({
+    message: 'Get table course',
+    status: HTTP_STATUS.OK,
+    data
+  })
+}
+
+export const deleteTeacherInCoureseController = async (req: Request, res: Response) => {
+  const { courseId, teacherId } = req.body
+  const id = new ObjectId(courseId)
+
+  const data = await courseServices.deleteTecherInCourse(id, String(teacherId))
+
+  res.json({
+    message: 'delete teacher in courses successfully',
+    status: HTTP_STATUS.OK,
+    data
+  })
+}
+
+export const deleteEnrollmentInCoureseController = async (req: Request, res: Response) => {
+  const { courseId, enrollmentId } = req.body
+  const id = new ObjectId(courseId)
+
+  const data = await courseServices.deleteEnrollmentInCourse(id, String(enrollmentId))
+
+  res.json({
+    message: 'delete enrollment in courses successfully',
+    status: HTTP_STATUS.OK,
+    data
+  })
+}
+
+export const addEnrollmentInCoureseController = async (req: Request, res: Response) => {
+  const { courseId, enrollmentId } = req.body
+  const id = new ObjectId(courseId)
+
+  const data = await courseServices.addEnrollmentInCourse(id, String(enrollmentId))
+
+  res.json({
+    message: 'add enrollment in courses successfully',
+    status: HTTP_STATUS.OK,
+    data
+  })
+}
+
+export const addTeacherInCoureseController = async (req: Request, res: Response) => {
+  const { courseId, teacherId } = req.body
+  const id = new ObjectId(courseId)
+
+  const data = await courseServices.addTeacherInCourse(id, String(teacherId))
+
+  res.json({
+    message: 'add teacher in courses successfully',
+    status: HTTP_STATUS.OK,
+    data
+  })
+}
+
+export const deleteSomeTeacherInCoureseController = async (req: Request, res: Response) => {
+  const { courseId, teacherIds } = req.body
+  const id = new ObjectId(courseId)
+
+  const data = await courseServices.delteSomeTeacherInCourse(id, teacherIds)
+
+  res.json({
+    message: 'delete some teachers in courses successfully',
+    status: HTTP_STATUS.OK,
+    data
+  })
+}
+
+export const deleteSomeEnrollmentsInCoureseController = async (req: Request, res: Response) => {
+  const { courseId, enrollmentIds } = req.body
+  const id = new ObjectId(courseId)
+
+  const data = await courseServices.deleteSomeEnrollmentsInCourse(id, enrollmentIds)
+
+  res.json({
+    message: 'delete some enrollments in courses successfully',
+    status: HTTP_STATUS.OK,
+    data
+  })
+}
+
+export const addSomeEnrollmentsInCoureseController = async (req: Request, res: Response) => {
+  const { courseId, enrollmentIds } = req.body
+  const id = new ObjectId(courseId)
+
+  const data = await courseServices.addSomeEnrollmentsInCourse(id, enrollmentIds)
+
+  res.json({
+    message: 'add some enrollments in courses successfully',
+    status: HTTP_STATUS.OK,
+    data
+  })
+}
+
+export const addSomeTeachersInCoureseController = async (req: Request, res: Response) => {
+  const { courseId, teacherIds } = req.body
+  const id = new ObjectId(courseId)
+
+  const data = await courseServices.addSomeTeachersInCourse(id, teacherIds)
+
+  res.json({
+    message: 'add some teachers in courses successfully',
+    status: HTTP_STATUS.OK,
+    data
+  })
+}
+
+export const paginationCourseController = async (req: Request, res: Response) => {
+ 
+  const page = parseInt(req.query.page as string) || 1;
+  const limit = parseInt(req.query.perPage as string) || 10; 
+
+
+  const data = await courseServices.paginatioCourseService(page,limit)
+
+  res.json({
+    message: 'pagination course',
+    status: HTTP_STATUS.OK,
+    page: page,
+    perPage: limit,
+    totalItems: data.length,
+    data
+  })
+}
--- src/routes/courses.routes.ts
+++ src/routes/courses.routes.ts
@@ -3,20 +3,50 @@ import { accessTokenValidation } from '~/middlewares/users.middlewares'
 import {
   addCourseValidation,
   updateCourseValidation,
-  deleteCourseValidation,
-  getCourseValidation
+  getCourseValidation,
+  courseValidation,
+  forAdminValidation,
 } from '~/middlewares/courses.middlewares'
 import {
   addCourseController,
   getCourseController,
   updateCourseController,
-  deleteCourseController
+  deleteCourseController,
+  getCourseForAdminController,
+  addEnrollmentInCoureseController,
+  deleteTeacherInCoureseController,
+  deleteEnrollmentInCoureseController,
+  addTeacherInCoureseController,
+  deleteSomeTeacherInCoureseController,
+  deleteSomeEnrollmentsInCoureseController,
+  addSomeEnrollmentsInCoureseController,
+  addSomeTeachersInCoureseController,
+  paginationCourseController,
 } from '~/controllers/courses.controllers'
 import { wrapRequestHandler } from '~/utils/handler'
 
 const coursesRouter = express.Router()
 
+
+coursesRouter.get('/',accessTokenValidation,forAdminValidation,wrapRequestHandler(paginationCourseController))
 coursesRouter.post('/add', accessTokenValidation, addCourseValidation, wrapRequestHandler(addCourseController))
+coursesRouter.get('/getCourseForAdmin', accessTokenValidation,wrapRequestHandler(getCourseForAdminController))
+
+
+
+coursesRouter.post('/addEnrollmentInCourse',accessTokenValidation,courseValidation,wrapRequestHandler(addEnrollmentInCoureseController));
+coursesRouter.post('/addSomeEnrollmentsInCourse',accessTokenValidation,courseValidation,wrapRequestHandler(addSomeEnrollmentsInCoureseController));
+coursesRouter.post('/addTeacherInCourse',accessTokenValidation,courseValidation,wrapRequestHandler(addTeacherInCoureseController));
+coursesRouter.post('/addSomeTeachersInCourse',accessTokenValidation,courseValidation,wrapRequestHandler(addSomeTeachersInCoureseController));
+
+
+coursesRouter.delete('/deleteTeacherInCourse',accessTokenValidation,courseValidation,wrapRequestHandler(deleteTeacherInCoureseController));
+coursesRouter.delete('/deleteEnrollmentInCourse',accessTokenValidation,courseValidation,wrapRequestHandler(deleteEnrollmentInCoureseController));
+coursesRouter.delete('/deleteSomeTeacherInCourse',accessTokenValidation,courseValidation,wrapRequestHandler(deleteSomeTeacherInCoureseController));
+coursesRouter.delete('/deleteSomeEnrollmentsInCourse',accessTokenValidation,courseValidation,wrapRequestHandler(deleteSomeEnrollmentsInCoureseController));
+
+
+coursesRouter.delete('/deleteCourse', accessTokenValidation, courseValidation, wrapRequestHandler(deleteCourseController))
 coursesRouter.get('/:slug', accessTokenValidation, getCourseValidation, wrapRequestHandler(getCourseController))
 coursesRouter.patch(
   '/update',
@@ -24,6 +54,6 @@ coursesRouter.patch(
   updateCourseValidation,
   wrapRequestHandler(updateCourseController)
 )
-coursesRouter.delete('/:id', accessTokenValidation, deleteCourseValidation, wrapRequestHandler(deleteCourseController))
+
 
 export default coursesRouter
